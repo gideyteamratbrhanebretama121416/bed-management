028c5993fc13733748bded0f6c4714b62ec4639f
hp <bretama99@gmail.com>
2023-11-05 09:47:01 +0300

    user management and redirection added

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -13,10 +13,10 @@ class UserPolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
-        return $user->hasRole(['Admin']);
-    }
+    // public function viewAny(User $user): bool
+    // {
+    //     return $user->hasRole(['Admin']);
+    // }
 
     // /**
     //  * Determine whether the user can view the model.
@@ -29,33 +29,33 @@ class UserPolicy
     // /**
     //  * Determine whether the user can create models.
     //  */
-    public function create(User $user): bool
-    {
-        //
-        return $user->hasRole(['Admin']);
+    // public function create(User $user): bool
+    // {
+    //     //
+    //     return $user->hasRole(['Admin']);
 
-    }
+    // }
 
     // /**
     //  * Determine whether the user can update the model.
     //  */
-    public function update(User $user, User $model): bool
-    {
-        //
+    // public function update(User $user, User $model): bool
+    // {
+    //     //
 
-        return $user->hasRole('Admin');
+    //     return $user->hasRole('Admin');
 
-    }
+    // }
 
     // /**
     //  * Determine whether the user can delete the model.
     //  */
-    public function delete(User $user, User $model): bool
-    {
-        //
-        return $user->hasRole('Admin');
+    // public function delete(User $user, User $model): bool
+    // {
+    //     //
+    //     return $user->hasRole('Admin');
 
-    }
+    // }
 
     // /**
     //  * Determine whether the user can restore the model.
